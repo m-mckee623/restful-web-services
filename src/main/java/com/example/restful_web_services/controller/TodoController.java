34450393e024db2a4610f4ab0ed77061a335bba6
Matthew <matthew.mckee.2018@gmail.com>
2024-10-16 21:30:25 +0200

    Cross Origin added to enable commmunication to frontend.

--- src/main/java/com/example/restful_web_services/controller/TodoController.java
+++ src/main/java/com/example/restful_web_services/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.example.restful_web_services.controller;
 import com.example.restful_web_services.pojo.Todo;
 import com.example.restful_web_services.service.TodoHardcodedService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class TodoController {
 
     @Autowired
