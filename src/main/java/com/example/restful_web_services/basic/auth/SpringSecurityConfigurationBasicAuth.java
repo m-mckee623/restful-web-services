2f215640651e13f388074105c6e135adb617d8b2
Matthew <matthew.mckee.2018@gmail.com>
2024-11-12 13:58:55 +0100

    Edited POM and Basic Authentication class.

--- src/main/java/com/example/restful_web_services/basic/auth/SpringSecurityConfigurationBasicAuth.java
+++ src/main/java/com/example/restful_web_services/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SpringSecurityConfigurationBasicAuth{
 
     @Bean
@@ -29,6 +31,7 @@ public class SpringSecurityConfigurationBasicAuth{
                         .sessionManagement(
                                 session -> session.sessionCreationPolicy
                                         (SessionCreationPolicy.STATELESS))
+                        .csrf().disable()
                         .build();
     }
 }
