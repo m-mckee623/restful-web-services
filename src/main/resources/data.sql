b6832199ccfb526534d3205bacf3ae660e72c2cf
Matthew <matthew.mckee.2018@gmail.com>
2025-02-03 18:45:27 +0100

    Made cleaner + updated data file.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into todo(id,username,description,target_date,is_done)
+values(10001,'tom','Remember to submit project',CURRENT_DATE,false);
+insert into todo(id,username,description,target_date,is_done)
+values(10002,'tom','Upload code to git',CURRENT_DATE,false);
+insert into todo(id,username,description,target_date,is_done)
+values(10003,'tom','Football tonight',CURRENT_DATE,false);
